b1b0955478703f3aa9f6361d7bb13bf4bfe059df
Prince Pardhu <princepardhu28@gmail.com>
2025-07-02 16:12:43 +0530

    update

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  extends: [
+    "next/core-web-vitals",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:tailwindcss/recommended",
+    "prettier",
+  ],
+  plugins: ["tailwindcss"],
+};
